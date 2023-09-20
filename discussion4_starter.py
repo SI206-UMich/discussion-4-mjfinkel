a02c7566d110e6c036b46285ea17afb218b651fe
mjfinkel <mjkfinkel@gmail.com>
2023-09-20 15:18:09 -0400

    Updated 092023 V1

--- discussion4_starter.py
+++ discussion4_starter.py
@@ -6,7 +6,9 @@ class Rectangle():
     # It sets an instance variable, "height" to the passed argument, height
 
     # YOUR CODE HERE
-
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
     # Create the "__str__" method
@@ -15,7 +17,8 @@ class Rectangle():
     #       "A rectangle with width ____ and height ____"
 
     # YOUR CODE HERE
-
+    def __str__(self):
+        return "A rectangle with width " + self.width + " and height " + self.height
 
 
     # Create the "verify_input" method
@@ -25,7 +28,8 @@ class Rectangle():
     #       False otherwise
 
     # YOUR CODE HERE
-
+    def verify_input(self):
+        return self.width > 0 and self.height > 0
 
 
     # Create the "area" method
@@ -34,7 +38,8 @@ class Rectangle():
     # Otherwise, it returns the area of the rectangle.
 
     # YOUR CODE HERE
-
+    def area(self):
+        pass
 
 
     # Create the "perimeter" method
